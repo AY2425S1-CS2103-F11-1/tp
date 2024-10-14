5a62e2fc2454cc1c71cb790ed90446834dab2de3
avrilmohh <avrilmoh1234@gmail.com>
2024-10-15 02:19:25 +0800

    Fix checkstyle requirements

--- src/main/java/seedu/address/logic/commands/EditAssignmentCommand.java
+++ src/main/java/seedu/address/logic/commands/EditAssignmentCommand.java
@@ -6,18 +6,35 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.assignment.Assignment;
 
+/**
+ * Edits the submission status of an existing assignment in the list.
+ */
 public class EditAssignmentCommand extends Command {
+
     public static final String COMMAND_WORD = "edit_assignment";
+
     public static final String MESSAGE_SUCCESS = "Successfully updated the submission status of assignment";
     public static final String MESSAGE_ASSIGNMENT_NOT_FOUND = "There is no such assignment in the list!";
+
     private final Assignment toEdit;
 
-    public EditAssignmentCommmand(Assignment assignment) {
+    /**
+     * Constructs an EditAssignmentCommand to edit the submission status of the specified {@code Assignment}.
+     *
+     * @param assignment The assignment whose submission status is to be edited.
+     */
+    public EditAssignmentCommand(Assignment assignment) {
         requireNonNull(assignment);
         toEdit = assignment;
-        // maybe can change this to index of assignment in the list instead
     }
 
+    /**
+     * Executes the command and updates the submission status of the assignment in the model.
+     *
+     * @param model The model which contains the list of assignments.
+     * @return A CommandResult indicating the success of the operation.
+     * @throws CommandException if the assignment is not found in the model.
+     */
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         if (!model.hasAssignment(toEdit)) {
